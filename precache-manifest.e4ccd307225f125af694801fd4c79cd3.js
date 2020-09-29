7dad474fa9fa33eee5f3b00b01ab7a78279b7c5e
Angie Araya <angiearaya@hotmail.com>
2020-09-29 09:49:23 +0200

    Updates

---
+++ precache-manifest.e4ccd307225f125af694801fd4c79cd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d6742029df35d35281604cd79b3b06a",
+    "url": "/js-lessons-deploy/index.html"
+  },
+  {
+    "revision": "6ffdc837c02ddf4efe77",
+    "url": "/js-lessons-deploy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b39cbd703bfe923f9f50",
+    "url": "/js-lessons-deploy/static/js/2.af32eef5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js-lessons-deploy/static/js/2.af32eef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ffdc837c02ddf4efe77",
+    "url": "/js-lessons-deploy/static/js/main.452f4123.chunk.js"
+  },
+  {
+    "revision": "0181da9fa359fbf16dd7",
+    "url": "/js-lessons-deploy/static/js/runtime-main.a13cf2dc.js"
+  }
+]);
